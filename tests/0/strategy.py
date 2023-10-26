e2e536196a8fe566c2e5d16b178549eb1f76edb7
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-10-26 18:35:52 +0530

    remove unnecessary comment

--- tests/0/strategy.py
+++ tests/0/strategy.py
@@ -5,10 +5,3 @@ numbers = lists(one_of(integers(), floats()), max_size=MAX_SEQUENCE_LEN)
 threshold = floats(min_value=0.0, exclude_min=True)
 
 strategy = numbers, threshold
-
-# Property based testing
-
-# increasing threshold should not change True result
-# decreasing threshold should not change False result
-# result should eventually become False if we keep halving threshold (unless -inf...)
-# result should eventually become True if we keep doubling threshold (unless +inf...)
