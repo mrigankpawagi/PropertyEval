31e050a7252d65b6274d410d6a0956e7187c6e40
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-11-25 15:09:49 +0530

    fix

--- autogen/samples/eval.py
+++ autogen/samples/eval.py
@@ -18,18 +18,34 @@ def evaluate(model):
         
         try:
             with open(f"models/{model}/Mbpp_{task_id}/0.py", "r") as f:
-                raw_completion = f.read()
-            print(f"Testing {model} on {task_id} ({entry_point})...")
+                raw_completion = f.read() + "\n"
+
         except Exception as e:
             continue # skip if this task is not in the model
         
         entry_point_index = raw_completion.find(f"def {entry_point}")
         
-        # remove any def statements after the entry point definition
-        if raw_completion.find("def ", entry_point_index + 1) != -1:
-            completion = raw_completion[:raw_completion.find("def ", entry_point_index + 1)]
-        else:
-            completion = raw_completion
+        # remove any statements after the entry point definition
+        new_line_positions = [i for i, c in enumerate(raw_completion) if c == "\n" and i > entry_point_index]
+        if len(new_line_positions) > 0:
+            function_end_index = new_line_positions[-1]
+            for i in new_line_positions:
+                # check if this line starts with indentation
+                if i + 1 < len(raw_completion) and raw_completion[i + 1] != " ":
+                    function_end_index = i
+                    break
+            completion = raw_completion[:function_end_index]
+
+        # remove anything before the entry point definition that is not an import or another function definition
+        # this is to remove stray text or comments
+        new_line_positions = [i for i, c in enumerate(completion) if c == "\n" and i < entry_point_index]
+        if len(new_line_positions) > 0:
+            code_start_index = new_line_positions[-1]
+            for i in new_line_positions:
+                if completion[i: i + 4] == "def " or completion[i: i + 7] == "import " or completion[i: i + 5] == "from ":
+                    code_start_index = i
+                    break
+            completion = completion[code_start_index:]               
             
         base = base_test(completion, test_list, entry_point)
         properteval = properteval_test(completion, strategy, code, entry_point, task_id)
